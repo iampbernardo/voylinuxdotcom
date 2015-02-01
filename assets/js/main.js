2723f81921d0b34c781f20686625abd57f97dffb
Pablo Bernardo <voylinux@gmail.com>
2015-02-01 19:39:14 +0100

    redesing: Index default layout finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,25 @@
  */
 
 (function(){
-  
+  'use strict';
+
+
+  var primaryNav = $('#primary_nav');
+  var menuButton = $('#menu_button');
+  var speed = 200;
+    menuButton.on('click', toggleMenu);
+
+  function toggleMenu(speed){
+    if(primaryNav.hasClass('open')){
+      primaryNav.animate({
+        'max-height': '0px'
+      }, speed);
+    }else{
+      primaryNav.animate({
+        'max-height': '1000px'
+      }, speed);
+    }
+    primaryNav.toggleClass('open');
+  }
+
 })();
